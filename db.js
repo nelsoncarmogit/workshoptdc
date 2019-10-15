424d675700da288ba72db0cbac8cdbf730cec0e5
nelsoncarmogit <nelsoncarmo@gmail.com>
2019-10-15 07:31:18 -0300

    Versão 1

---
+++ db.js
@@ -0,0 +1,27 @@
+var mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb+srv://nelson:nelson@cluster0-c3mwl.mongodb.net/test?retryWrites=true&w=majority")
+            .then(conn => global.conn = conn.db("workshoptdc"))
+            .catch(err => console.log(err))
+
+ 	
+function findAll(callback){  
+  global.conn.collection("clientes").find({}).toArray(callback);
+}
+
+function insert(cliente, callback){
+    global.conn.collection("clientes").insert(cliente, callback);
+}
+
+var ObjectId = require("mongodb").ObjectId;
+function findOne(id, callback){  
+    global.conn.collection("clientes").find(new ObjectId(id)).toArray(callback);
+}
+
+function update(id, cliente, callback){
+    global.conn.collection("clientes").update({_id:new ObjectId(id)}, cliente, callback);
+}
+
+function deleteOne(id, callback){
+    global.conn.collection("clientes").deleteOne({_id: new ObjectId(id)}, callback);
+}
+module.exports = { findAll, insert, findOne, update, deleteOne }
